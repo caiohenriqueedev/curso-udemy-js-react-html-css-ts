8dfd62103e3351e28f12b0ce09d66fe5e5d4011d
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-04-02 19:18:52 -0300

    adicionando o seção sobre estruturas e funções no JS (falta terminar o resto do arquivo scripts.js, irei terminar depois e fazer o commit posteriormente

---
+++ 15_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,43 @@
+// 1 - variavéis
+let nome = "Matheus";
+
+// ponto e virgula e opcional
+console.log(nome);
+
+nome = "Matheus Battisti"
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32;
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - mais sobre variavéis
+// let 2teste = "invalido"
+// let @teste = "invalido"
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+
+const nomeCompleto = "João da Silva";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok"
+
+let $teste = "ok"
+
+console.log(_teste, $teste);
+
+// 
