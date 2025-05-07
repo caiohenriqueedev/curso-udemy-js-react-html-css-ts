07a6c67505e12085fc70a3ab3a36aa04cdae5bd8
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-05-07 00:01:06 -0300

    adicionando seção de arrays e objetos em JS (com join, split, trim, padstart e padend), falta o finalzinho da seção, depois irei committar

--- 17_ARRAYS_E_OBJS/js/scripts.js
+++ 17_ARRAYS_E_OBJS/js/scripts.js
@@ -172,7 +172,6 @@ letters.unshift("z");
 
 console.log(letters);
 
-<<<<<<< HEAD
 // 13 - indexOf e lastIndexOf
 const myElements = ["Morango", "Maçã", "Abacate", "Perâ", "Abacate"];
 
@@ -283,9 +282,3 @@ const itensParaComprar = ["Mouse", "Teclado","Monitor"];
 const fraseDeCompra = `Precisamos comprar: ${itensParaComprar.join(",")}.`;
 
 console.log(fraseDeCompra);
-=======
-// 13 - IndexOf e lastIndexOf
-const MyElements = ["Morango", "Maçã", "Abacate", "Pêra", "Abacate"];
-
-console.log(MyElements.indexOf("Maçã"));
->>>>>>> e7b169b6b9f15bca2281a8e2475672c1f2d2f2b5
