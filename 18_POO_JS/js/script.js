2ca0ee52b652b8195dbf4f04e16003be9eb26fe0
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-05-13 21:10:16 -0300

    adicionando seção de funções metodos e propiedades de POO em Js, falta um pedaço ainda da seção, irei posteriormente comitta-la

--- 18_POO_JS/js/script.js
+++ 18_POO_JS/js/script.js
@@ -116,3 +116,106 @@ Cachorro.prototype.uivar = function(){
     console.log(Cachorro.prototype);
 
     husky.uivar();
+
+    // 9 - classes es6
+    class CachorroClasse {
+        constructor(nome, raca){
+            this.nome = nome
+            this.raca = raca
+        }
+    }
+
+    const jeff = new CachorroClasse("Jeff", "Labrador");
+
+    console.log(jeff);
+
+    console.log(Object.getPrototypeOf(jeff));
+
+    // 10 - mais sobre classes
+    class Caminhao {
+        constructor(eixos, cor){
+            this.eixos = eixos
+            this.cor = cor
+
+        }
+        descreverCaminhao(){
+            console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`);
+        }
+    }
+
+    const scania = new Caminhao(6, "Vermelha");
+
+    console.log(scania)
+
+    scania.descreverCaminhao();
+
+    Caminhao.motor = 4
+
+    const c2 = new Caminhao(4, "Preta");
+
+    console.log(c2);
+
+    console.log(c2.motor);
+
+    Caminhao.prototype.motor = 4.0;
+
+    const c3 = new Caminhao(6, "Azul");
+
+    console.log(c3);
+
+    // 11 - override
+    class Humano {
+        constructor(nome, idade){
+    this.nome = nome
+    this.idade = idade
+
+    }   
+}
+
+const matheus = new Humano("Matheus", 31);
+
+console.log(matheus);
+
+Humano.prototype.idade = "Não definida"
+
+console.log(matheus.idade);
+
+console.log(Humano.prototype.idade);
+
+// 12 - symbol
+class Aviao {
+    constuctor(marca, turbinas){
+        this.marca = marca
+        this.turbinas = turbinas
+    }
+}
+
+const asas = Symbol();
+const pilotos = Symbol ();
+
+Aviao.prototype[asas] = 2;
+
+Aviao.prototype[pilotos] = 3;
+
+const boeing = new Aviao("Boeing", 10);
+
+console.log(boeing);
+
+console.log(boeing[asas]);
+
+console.log(boeing[pilotos]);
+
+// 13 - getter e setter
+class Post {
+    constructor(titulo, descricao, tags){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.tags = tags;
+    }
+
+    get exibirTitulo() {
+        return `Você está lendo: ${this.titulo}`;
+    }
+}
+
+const MyPost = new Post("Algum Post", "é um post sobre programação");
