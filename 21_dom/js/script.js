6d9dcdba81c3e4b569fafeaa36594621d69f7f05
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-05-28 19:45:20 -0300

    adicionando a seção de DOM (Js e o HTML) com appendchild, insertbefore, selectquery etc.

---
+++ 21_dom/js/script.js
@@ -0,0 +1,101 @@
+// 1 - movendo-se pelo DOM
+console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+// 2 - selecionando por tag
+const listItens = document.getElementsByTagName("li");
+
+console.log(listItens);
+
+// 3 - selecionando elemento por id
+const title = document.getElementById("title");
+
+console.log(title);
+
+// 4 - selecionando elementos por classe
+const products = document.getElementsByClassName("product");
+
+console.log(products);
+
+// 5 - selecionando os elementos por css
+const productsQuery = document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer);
+
+// 6 - insertBefore
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title);
+
+// 7 - appendChild
+const navLinks = document.querySelector("nav ul");
+
+const li = document.createElement("li");
+
+navLinks.appendChild(li);
+
+// 8 - replaceChild
+const h2 = document.createElement("h2");
+
+h2.textcontent = "Meu Novo titulo!";
+
+header.replaceChild(h2, title);
+
+// 9 - createTextNode
+const myText = document.createTextNode("agora vamos colocar mais um titulo");
+
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer.appendChild(h3);
+
+// 10 - trabalhando com atributos
+const firstLink = navLinks.querySelector("a");
+
+console.log(firstLink);
+
+firstLink.setAttribute("href", "https://www.google.com");
+
+console.log(firstLink.getAttribute("href"));
+
+firstLink.setAttribute("target", "_blank");
+
+// 11 - altura e largura
+const footer = document.querySelector("footer");
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth);
+console.log(footer.clientHeight);
+
+// 12 - posição do elemento
+const prodcut1 = products[0];
+
+console.log(prodcut1.getBoundingClientRect());
+
+// 13 - css com js
+mainContainer.style.color = "red";
+mainContainer.style.backgroundColor = "#333";
+mainContainer.style.paddingBottom = "15px";
+
+// 14 - alterando estilos de varios elementos
+for(const li of listItens){
+    li.style.backgroundColor = "red";
+}
