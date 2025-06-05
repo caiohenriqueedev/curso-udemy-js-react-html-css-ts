f310d8ac50705d7c0f227a409516ca50ac894b40
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2025-06-05 17:59:34 -0300

    adicionando o final da seção (projeto em Js de tabuada) #ufa

--- 23_projeto_tabuada_js/js/script.js
+++ 23_projeto_tabuada_js/js/script.js
@@ -1,39 +1,48 @@
-// Seleão de elementos
+// Selecão de elementos
 const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
-const multiplicationTable = document.querySelector("#multiplication-operations");
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
 
-
-
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
 
 // Funções
 const createTable = (number, multiplicatorNumber) => {
-    
-    multiplicationTable.innerHTML = "";
+  multiplicationTable.innerHTML = "";
 
-        for(i = 1; i <= multiplicatorNumber; i++){
-            
-        }
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
 
+    const template = `<div class ="row">
+            <div class= "operation"> ${number} x ${i} = </div>
+            <div class="result"> ${result}</div>
+        </div>`;
 
+    const parser = new DOMParser();
 
-}
+    const htmlTemplate = parser.parseFromString(template, "text/html");
 
+    const row = htmlTemplate.querySelector(".row");
 
+    multiplicationTable.appendChild(row);
+  }
 
-
+  multiplicationTitle.innerHTML = number;
+};
 
 // Eventos
 multiplicationForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const multiplicationNumber = +numberInput.value;
-
   const multiplicatorNumber = +multiplicationInput.value;
 
   if (!multiplicationNumber || !multiplicatorNumber) return;
 
-  console.log(multiplicationNumber, multiplicatorNumber);
+  createTable(multiplicationNumber, multiplicatorNumber);
 });
