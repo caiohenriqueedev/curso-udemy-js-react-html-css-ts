f5e2c3d062d91b3a591c32ebe282b5c2b9616645
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2026-01-16 23:59:49 -0300

    adicionando imagens ao react

--- 34_react__intro/3_avançado_react/avançando/src/App.jsx
+++ 34_react__intro/3_avançado_react/avançando/src/App.jsx
@@ -3,32 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// 2 - imagem em assets
+import noite from './assets/noite.jpg';
+
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+    <h1> Avançando em React</h1>
+    {/* 1 - imagem em public*/}
+    <img src="noite.jpg" alt="imagem da rua" />
+    {/* 2 - imagem em assets*/}
+    <img src={noite} alt="imagem da noite" />
+    </div>
   )
 }
 
