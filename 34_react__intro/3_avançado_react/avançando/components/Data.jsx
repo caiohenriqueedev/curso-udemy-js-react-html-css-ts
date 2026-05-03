84b2e52b88bf88904c99af3fe0dd85849300e7b0
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2026-05-03 20:38:03 -0300

    arrumando algumas tralhas/bagunças da seção avançado em react dentro do projeto do curso hehe, voltei depois um grande hiato e uma pequena pausa

---
+++ 34_react__intro/3_avançado_react/avançando/components/Data.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Data = () => {
+  let someData = 10;
+
+  return (
+    <div>
+      <div>
+        <p>Valor: {someData}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
--- 34_react__intro/3_avançado_react/avançando/src/App.jsx
+++ 34_react__intro/3_avançado_react/avançando/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 // 2 - imagem em assets
-import noite from './assets/noite.jpg';
+import noite from "./assets/noite.jpg";
+
+// 3 - useState
+import Data from "../components/Data";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
   return (
-    <div className="App">
-    <h1> Avançando em React</h1>
-    {/* 1 - imagem em public*/}
-    <img src="noite.jpg" alt="imagem da rua" />
-    {/* 2 - imagem em assets*/}
-    <img src={noite} alt="imagem da noite" />
+    <div className="App" style={{ paddingBottom: "500px" }}>
+      <h1> Avançando em React</h1>
+      {/* 1 - imagem em public*/}
+      <img src="/rua.jpg" alt="alguma imagem" />
+      {/* 2 - imagem em assets*/}
+      <img src={noite} alt="imagem da noite" />
+      {/* 3 - useState */}
+      <Data />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
