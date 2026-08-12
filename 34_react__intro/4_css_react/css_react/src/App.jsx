026920975c47b8c37002aa0c25aad1c6c2d1b43e
Caio Henrique Ramos de Souza <caiohenriquebr12@gmail.com>
2026-08-12 02:53:13 -0300

    adicionando o final e resto da seção de css com react (css inline, dinamico, modules etc.)

--- 34_react__intro/4_css_react/css_react/src/App.jsx
+++ 34_react__intro/4_css_react/css_react/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 
 // 2  css de componente
-import MyComponents from './MyComponents'
+import MyComponents from './components/MyComponents';
+import Title from './components/Title';
 
 function App() {
+  // 4 - css inline dinamico
+  const n = 15;
+
+  // 5 - classes dinamicas
+  const redTitle = true;
 
   return (
     <div className="App">
@@ -11,6 +17,20 @@ function App() {
       <h1>Css global</h1>
       {/* 2 - css de componente */}
       <MyComponents />
+      <p> Pegou o CSS do componente</p>
+      {/* 3 - css inline */} 
+      <p style={{ color: "blue", padding: "25px", borderTop: "1px dotted blue"}}>Este é um parágrafo com CSS inline</p>
+    {/* 4 - inline style dinâmico */}
+    <h2 style={n > 10 ? {color: "purple"} : {color: "magenta"}}>
+      Este é um título dinâmico
+    </h2>
+    <h2 style={n > 20 ? {color: "purple"} : {color: "magenta"}}>
+      Este é um título dinâmico
+    </h2>
+    {/* 5 - classe dinâmica */}
+    <h2 className={redTitle ? "red-title" : ""}>Este é um título com classe dinâmica</h2>
+    {/* 6 - CSS modules */}
+    <Title />
     </div>
   )
 }
---
+++ 34_react__intro/4_css_react/css_react/src/components/Title.jsx
@@ -0,0 +1,9 @@
+import styles from './title.module.css';
+
+const Title = () => {
+  return (
+    <h1 className={styles.title}> Meu titulo</h1>
+  );
+};
+
+export default Title;
